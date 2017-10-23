ca3d358bed699f9edee0b11c0219847d7231cc96
Almir Junior <junior_fagundes_7@hotmail.com>
2017-10-23 15:52:13 -0200

    Commit Inicial

---
+++ QRCode/QRCode/ImageConvertor.cs
@@ -0,0 +1,131 @@
+﻿using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+
+namespace QRCode
+{
+    class ImageConvertor :  IDisposable
+    {
+
+
+
+        public static string ConvertPDFToImage(PdfReader reader, string diretorioSaida)
+        {
+            PdfReaderContentParser parser = new PdfReaderContentParser(reader);
+            ImageRenderListener listener = new ImageRenderListener();
+            try
+            {
+                parser.ProcessContent(1, listener);
+                return ConvertJpgToPng(0, listener, diretorioSaida);
+            }
+            finally
+            {
+
+                GC.SuppressFinalize(listener);
+                GC.SuppressFinalize(parser);
+                GC.SuppressFinalize(reader);
+                reader.Dispose();
+            }
+        }
+
+
+        public static IEnumerable<string> ConvertPDFToImage(PdfReader reader, string diretorioSaida, int quantidadePaginas)
+        {
+            PdfReaderContentParser parser = new PdfReaderContentParser(reader);
+            ImageRenderListener listener = new ImageRenderListener();
+            List<string> listaDeCaminhos = new List<string>();
+            try
+            {
+                for (int i = 1; i <= quantidadePaginas; i++)
+                {
+                    parser.ProcessContent(i, listener);
+                }
+
+                for (int i = 0; i < listener.Images.Count; ++i)
+                {
+                    listaDeCaminhos.Add(ConvertJpgToPng(i, listener, diretorioSaida));
+
+                }
+                return listaDeCaminhos;
+            }
+            finally
+            {
+
+                GC.SuppressFinalize(listener);
+                GC.SuppressFinalize(parser);
+                GC.SuppressFinalize(reader);
+                GC.SuppressFinalize(listaDeCaminhos);
+                reader.Dispose();
+            }
+
+        }
+
+
+
+
+        private static string ConvertPngToJpg(int i, ImageRenderListener listener, string diretorio)
+        {
+            System.Drawing.Imaging.EncoderParameters parms = new System.Drawing.Imaging.EncoderParameters(1);
+            parms.Param[0] = new System.Drawing.Imaging.EncoderParameter(System.Drawing.Imaging.Encoder.Compression, 0);
+
+            System.Drawing.Imaging.ImageCodecInfo jpegEncoder = ImageCodecInfo.GetImageEncoders().Single(p => p.CodecName.Contains("JPEG"));
+            System.Drawing.Image img = System.Drawing.Image.FromStream(new MemoryStream(listener.Images[i]));
+            string path = diretorio + "\\" + listener.ImageNames[i].ToLower().Replace(".png", ".jpg");
+            img.Save(path, jpegEncoder, parms);
+            return path;
+        }
+
+        private static string ConvertJpgToPng(int i, ImageRenderListener listener, string diretorio)
+        {
+            System.Drawing.Imaging.EncoderParameters parms = new System.Drawing.Imaging.EncoderParameters(1);
+            parms.Param[0] = new System.Drawing.Imaging.EncoderParameter(System.Drawing.Imaging.Encoder.Compression, 0);
+
+            System.Drawing.Imaging.ImageCodecInfo jpegEncoder = ImageCodecInfo.GetImageEncoders().Single(p => p.CodecName.Contains("PNG"));
+            System.Drawing.Image img = System.Drawing.Image.FromStream(new MemoryStream(listener.Images[i]));
+            string path = diretorio + "\\" + i +listener.ImageNames[i].ToLower().Replace(".jpg", ".png");
+            img.Save(path, jpegEncoder, parms);
+            return path;
+        }
+
+        public void Dispose()
+        {
+
+            GC.SuppressFinalize(this);
+        }
+    }
+
+    class ImageRenderListener : IRenderListener
+    {
+        public void RenderText(TextRenderInfo renderInfo) { }
+        public void BeginTextBlock() { }
+        public void EndTextBlock() { }
+
+        public List<byte[]> Images = new List<byte[]>();
+        public List<string> ImageNames = new List<string>();
+        public void RenderImage(ImageRenderInfo renderInfo)
+        {
+            PdfImageObject image = null;
+            try
+            {
+                image = renderInfo.GetImage();
+                if (image == null) return;
+
+                ImageNames.Add(string.Format(
+                  "qrcode.{0}", image.GetFileType()
+                ));
+                using (MemoryStream ms = new MemoryStream(image.GetImageAsBytes()))
+                {
+                    Images.Add(ms.ToArray());
+                }
+            }
+            catch (IOException ie)
+            {
+                throw ie;
+            }
+        }
+    }
+}
---
+++ QRCode/QRCode/Leitura.cs
@@ -0,0 +1,101 @@
+﻿using iTextSharp.text.pdf;
+using MessagingToolkit.QRCode.Codec;
+using MessagingToolkit.QRCode.Codec.Data;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+
+namespace QRCode
+{
+    public class Leitura : IDisposable
+    {
+        public string imagem { get; set; }
+        public List<string> caminhos { get; set; }
+
+        public string LerUmQRCode(string caminhoPDF, string diretorio)
+        {
+            this.imagem = ImageConvertor.ConvertPDFToImage(new PdfReader(caminhoPDF), diretorio);
+            Bitmap bitmap = new Bitmap(System.Drawing.Image.FromFile(imagem));
+            QRCodeDecoder dec = new QRCodeDecoder();
+            try
+            {
+                var x = dec.decode(new QRCodeBitmapImage(bitmap));
+                //return (dec.decode(new QRCodeBitmapImage(bitmap))).Replace("tem sim", "NÂO tem");
+                return x.ToString();
+            }
+            finally
+            {
+                bitmap.Dispose();
+                GC.SuppressFinalize(bitmap);
+                GC.SuppressFinalize(dec);
+                GC.SuppressFinalize(bitmap);
+                File.Delete(caminhoPDF);
+            }
+
+        }
+        public Dictionary<string, string> LerVariosQRCode(string caminhoPDF, string diretorio)
+        {
+            Dictionary<string, string> output = new Dictionary<string, string>();
+            caminhos = ImageConvertor.ConvertPDFToImage(new PdfReader(caminhoPDF), diretorio, new PdfReader(caminhoPDF).NumberOfPages).ToList();
+            MessagingToolkit.QRCode.Codec.QRCodeDecoder dec = new MessagingToolkit.QRCode.Codec.QRCodeDecoder();
+            try
+            {
+                foreach (var caminho in caminhos)
+                {
+
+                    try
+                    {
+                        Bitmap bitmap = new Bitmap(System.Drawing.Image.FromFile(caminho));
+                        output.Add(caminho, dec.decode(new MessagingToolkit.QRCode.Codec.Data.QRCodeBitmapImage(bitmap)));
+                        bitmap.Dispose();
+                        GC.SuppressFinalize(bitmap);
+                    }
+                    catch (Exception)
+                    {
+                        continue;
+                    }
+
+                    
+
+                }
+                return output;
+
+            }
+            finally
+            {
+                GC.SuppressFinalize(dec);
+                GC.SuppressFinalize(output);
+                
+                File.Delete(caminhoPDF);
+
+            }
+
+
+
+        }
+        public void Dispose()
+        {
+
+
+            if (imagem != null)
+            {
+
+                GC.SuppressFinalize(imagem);
+                File.Delete(imagem);
+
+            }
+
+            if (caminhos != null)
+            {
+                caminhos.ForEach(item => File.Delete(item));
+                GC.SuppressFinalize(caminhos);
+            }
+
+
+            GC.SuppressFinalize(this);
+        }
+    }
+
+}
